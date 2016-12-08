d203f4f0aa77877f7e8aad9454dd03804dd8c712
okulkar2 <omkr22@gmail.com>
2016-12-08 18:59:12 -0500

    Flag.java updated added ALUAvailable, MULAvailable & LSFUAvailable

--- Project1/src/simulator/Flag.java
+++ Project1/src/simulator/Flag.java
@@ -8,6 +8,33 @@ public class Flag {
 	private static boolean zeroFlag;
 	private static boolean stallFlag;
 	private static boolean branchFlag;
+	private static boolean ALUAvailable;
+	private static boolean MULAvailable;
+	private static boolean LSFUAvailable;
+
+	public static boolean isALUAvailable() {
+		return ALUAvailable;
+	}
+
+	public static void setALUAvailable(boolean ALUAvailableIn) {
+		ALUAvailable = ALUAvailableIn;
+	}
+
+	public static boolean isMULAvailable() {
+		return MULAvailable;
+	}
+
+	public static void setMULAvailable(boolean MULAvailableIn) {
+		MULAvailable = MULAvailableIn;
+	}
+
+	public static boolean isLSFUAvailable() {
+		return LSFUAvailable;
+	}
+
+	public static void setLSFUAvailable(boolean LSFUAvailableIn) {
+		LSFUAvailable = LSFUAvailableIn;
+	}
 
 	public static boolean isForwardEXFlagSet() {
 		return forwardEXFlag;
