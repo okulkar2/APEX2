a6c47514b985aea856f4e0a02853a10f19f21e18
chetankh23 <chetan.kh23@gmail.com>
2016-12-09 12:51:05 -0500

    Added multiply FU

---
+++ Project1/src/simulator/MultiplyFU.java
@@ -0,0 +1,37 @@
+package simulator;
+
+public class MultiplyFU {
+
+	private Pipeline pipeline;
+	private IHelper helper;
+	
+	public MultiplyFU(Pipeline pipelineIn, IHelper helperIn) {
+
+		pipeline=pipelineIn;
+		helper=helperIn;
+	}
+	
+	
+	public void performOperation() {
+		
+		// to do mul4 stage
+		
+		if(pipeline.getStages().get(Constants.MUL3)!=null) {
+			
+			pipeline.getStages().put(Constants.MUL4, pipeline.getStages().get(Constants.MUL3));
+			pipeline.getStages().put(Constants.MUL3, null);
+		}
+		
+		if(pipeline.getStages().get(Constants.MUL2)!=null) {
+			
+			pipeline.getStages().put(Constants.MUL3, pipeline.getStages().get(Constants.MUL2));
+			pipeline.getStages().put(Constants.MUL2, null);
+		}
+		
+		if(pipeline.getStages().get(Constants.MUL1)!=null) {
+			
+			pipeline.getStages().put(Constants.MUL2, pipeline.getStages().get(Constants.MUL1));
+			pipeline.getStages().put(Constants.MUL1, null);
+		}
+	}
+}
